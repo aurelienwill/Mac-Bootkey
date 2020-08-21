bd614ab713322b5c9d3098ba216f7e3326435e3a
Willemijns Aurélien <willemijns66@gmail.com>
2020-08-21 14:01:36 +0200

    Update Bootkey.sh

--- Bootkey.sh
+++ Bootkey.sh
@@ -1,50 +1,59 @@
-#!/bin/bash
+echo ' ____   _____  _____ _____      _   __ _____ _   __  _____ _   _   __   __  ___  _____ '
+echo '| ___ \|  _  ||  _  |_   _|    | | / /|  ___\ \ / / |  _  | \ | | |  \/  | / _ \/  __ \'
+echo '| |_/ /| | | || | | | | |______| |/ / | |__  \ V /  | | | |  \| | | .  . |/ /_\ \ /  \/'
+echo '| ___ \| | | || | | | | |______|    \ |  __|  \ /   | | | | . ` | | |\/| ||  _  | |    '
+echo '| |_/ /\ \_/ /\ \_/ / | |      | |\  \| |___  | |   \ \_/ / |\  | | |  | || | | | \__/\'
+echo '\____/  \___/  \___/  \_/      \_| \_/\____/  \_/    \___/\_| \_/ \_|  |_/\_| |_/\____/'
 
-echo "Which distribution do you want to install?"
-echo "Write 1 for Ubuntu 18.04.3 LTS"
-echo "Write 2 for Manjaro XFCE 18.1.1"
-echo "Write 3 for Raspbian Version:September 2019"
-echo "Write 4 for Flash windows Iso"
-read response
-diskutil list physical
-echo "Write the disk on which the iso should be flashed ex :disk3 "
-read diskfl
-
- LaunchFlash () {
-        diskutil eraseDisk FAT32 $1 $diskfl
-        diskutil unmountDisk /dev/$diskfl
-        #if you want to see progress bar install pv with homebrews https://brew.sh/ comment l18 and uncomment l17
-        #pv $1.iso | dd of=/dev/$diskfl
-        sudo dd if=$1.iso of=/dev/$diskfl bs=1m
-        diskutil eject /dev/$diskfl
-        rm -i $1.iso 
-        echo "Finish"
+ExistFile()
+{
+	if [ -e $1 ]
+		then
+			result='true'
+	else
+		result='false'
+			echo "File not found"
+			exit
+			fi
 }
+if [ -z $1 ]
+	then
+		ls *.iso
+		echo " Please enter an ImageFile of Windows (with extension)"
+		read WindowsImage
+		ExistFile WindowsImage
+		
+else
+	ExistFile $1
+	WindowsImage=$1
+fi 
 
-if [ $response  -eq 1 ]
-    then
-        curl http://releases.ubuntu.com/18.04.3/ubuntu-18.04.3-desktop-amd64.iso?_ga=2.67788756.45556580.1571741444-940794574.1571741444 > UBUNTU.iso
-        LaunchFlash UBUNTU
-elif [ $response  -eq 2 ]
-    then
-        curl https://dotsrc.dl.osdn.net/osdn/storage/g/m/ma/manjaro/xfce/18.1.1/manjaro-xfce-18.1.1-191015-linux53.iso > MANJARO.iso
-        LaunchFlash MANJARO
-elif [ $response  -eq 3 ]
-    then
-         curl https://downloads.raspberrypi.org/raspbian_latest > RASPBIAN.iso
-        LaunchFlash RASPBIAN   
-elif [ $response  -eq 4 ]
-    then 
-    echo "What is the file name?(With extension ex: windows.iso)"
-    read isoname
-    hdiutil mount $isoname.iso
-    df -h
-    echo "What is the name of the windows volume mounted ex: /VOLUMES/CCCCOMA_X64FRE_FR-FR_DV9 (enter only name without /VOLUMES/)"
-    read namemounted
-    echo "Do you want to install the USB key in GPT or MBR"
-    read biostable
-    diskutil eraseDisk MS-DOS "WINDOWS" $biostable $diskfl
-    cp -rpv /VOLUMES/$namemounted/* /Volumes/WINDOWS/
-    
+#Montage de l'iso avec hdiutils / On enleve les espace & on cut pour avoir Le nom du disk sur lequel a été monter l'iso & le nom du volume
+MountIso=$(hdiutil attach $WindowsImage | tr -s ' ')                                                                                           
+DiskMounted=$(cut -d" " -f1 <<<$MountIso)
+VolumeMounted=$(cut -d" " -f2 <<<$MountIso)
+#Sur la dernier ligne que la commande Mount nous donne ,  on va chercher le dernier volume Monter & comparer dés que la clefs usb sera inserer.)
+DiskList=$(mount |awk 'END {print}')
+CompareDiskList=$DiskList
+while [ "$DiskList" = "$CompareDiskList" ]  
+do
+	unset CompareDiskList
+	CompareDiskList=$(mount |awk 'END {print}')
+	echo "Insert the USBKEY /!/ Warning you will lost all your data on your usbkey"
+	sleep 3
+done
+USBVolume=$(mount |awk 'END {print}' | cut -d" " -f3)
+USBDisk=$(mount |awk 'END {print}' | cut -d" " -f1)
+#On verifie la taille du fichier WIM car si > 4go le Fat32 ne prends pas en charge ...
+WIMsize=du -sk "${USBMounted}/sources/install.wim" | awk '{print $1}'
+echo "Do you want to install the USB key in GPT or MBR"
+read PartitionTable
+diskutil eraseDisk MS-DOS "WINDOWS" $PartitionTable $USBDisk
+#Si le fichier >4Go on passe par Wimlib et on le split par paquet de 1go
+if [ "$WIMsize" > 4000000 ]
+then
+rsync -avh --progress --exclude=sources/install.wim $VolumeMounted $USBVolume
+wimlib-imagex split "${VolumeMounted}/sources/install.wim " "${USBVolume}/sources/install.swm" 1000
+else
+rsync -avh --progress $VolumeMounted $USBVolume
 fi
-
